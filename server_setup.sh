b791d63b0f68143ecb64bb2b2c8a54d2e7ddc040
singularis <singularis314@gmail.com>
2023-07-16 21:47:12 +0000

    Fixed scripts sequence

--- server_setup.sh
+++ server_setup.sh
@@ -1,5 +1,58 @@
 #!/bin/bash
 
+# Check if the user is root.
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be root to run this script."
+  exit 1
+fi
+
+#Install fish
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install fish
+
+# Check if kubectl is already installed.
+if kubectl version > /dev/null; then
+  echo "kubectl is already installed."
+  exit 0
+fi
+
+# Install kubectl using apt.
+sudo apt-get install -y ca-certificates curl
+sudo apt-get install -y apt-transport-https
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt install kubectl
+
+# Verify the installation.
+kubectl version
+
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+# Disable password authentication.
+
+# Get the current SSH port.
+current_port=$(grep 'Port ' /etc/ssh/sshd_config | awk '{print $2}')
+
+# Get the new SSH port.
+echo "Promt a new port: "
+read new_port
+
+# Check if the new SSH port is valid.
+if ! [[ $new_port -ge 1024 && $new_port -le 65535 ]]; then
+  echo "The new SSH port is not valid."
+  exit 1
+fi
+
+# Update the SSH port in the SSH configuration file.
+sed -i "s/$current_port/$new_port/g" /etc/ssh/sshd_config
+
+# Restart the SSH service.
+service ssh restart
+
+# Print a success message.
+echo "The SSH port has been successfully changed to $new_port."
+
 # Check if Python is already installed.
 if python --version > /dev/null; then
   echo "Python is already installed."
@@ -17,5 +70,8 @@ pip install ansible
 
 # Check the Ansible version.
 ansible --version
+
+# Restart the SSH service.
+service ssh restart
 
 ansible-playbook server_setup.yml
--- user_setup.sh
+++ user_setup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Check if the user is root.
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be root to run this script."
-  exit 1
-fi
+# Script should bes started in the current shell ". user_setup.sh"
 
 # Get the user's email address.
 echo "Enter your email address: "
@@ -13,11 +9,6 @@ read email
 # Get the user's name .
 echo "Enter your name: "
 read name
-
-#Install fish
-sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt update
-sudo apt install fish
 
 #Generate new SSH keys
 ssh-keygen -t rsa -b 4096 -C "$email"
@@ -28,36 +19,12 @@ ssh-add ~/.ssh/id_rsa
 echo "Your public key is:"
 cat ~/.ssh/id_rsa.pub
 
-# Disable password authentication.
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-
 # Prompt for the user's SSH key.
 echo "Enter your SSH key: "
 read ssh_key
 
 # Add the user's SSH key to the authorized_keys file.
-echo "$ssh_key" >> /etc/ssh/authorized_keys
-
-# Check if kubectl is already installed.
-if kubectl version > /dev/null; then
-  echo "kubectl is already installed."
-  exit 0
-fi
-
-# Install kubectl using apt.
-sudo apt-get install -y ca-certificates curl
-sudo apt-get install -y apt-transport-https
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt install kubectl
-
-# Verify the installation.
-kubectl version
-
-
-# Restart the SSH service.
-service ssh restart
+echo "$ssh_key" >> ~/.ssh/authorized_keys
 
 #Setup git
 git config --global user.email "$email"
