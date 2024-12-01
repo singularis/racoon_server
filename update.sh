5a811ddc090337bdd265e63ca0872ad56cf4d467
dante <singularis314@gmail.com>
2024-12-01 22:03:57 +0000

    Updater fixes

--- update.sh
+++ update.sh
@@ -16,9 +16,6 @@ apt-get update
 echo "Upgrading installed packages..."
 apt-get upgrade -y
 
-echo "Performing distribution upgrade..."
-apt-get dist-upgrade -y
-
 echo "Removing unnecessary packages..."
 apt-get autoremove -y
 
@@ -28,5 +25,6 @@ apt-get autoclean
 echo "Clean up unnecessary images..."
 sudo ctr -n=k8s.io images prune --all
 find . -type f -name ".attach_pid*" -exec rm -f {} \;
+crictl rmi --prune
 
 echo "Update and cleanup process complete!"
