292aa90aa6d48f951f6964f2dfd05a8c3bc05af1
dante <singularis314@gmail.com>
2025-02-09 18:14:05 +0000

    Samba

--- workload/kafka/restart.sh
+++ workload/kafka/restart.sh
@@ -4,12 +4,14 @@
 export KUBECONFIG=/home/dante/.kube/config
 
 # Remove Kafka and Zookeeper directories
-sudo rm -rf /other/kafka/kafka-1/*
-sudo rm -rf /other/zookeeper/zookeeper-1/*
+#sudo rm -rf /other/kafka/kafka-1/*
+#sudo rm -rf /other/zookeeper/zookeeper-1/*
 
 # Force delete the pods
 kubectl -n kafka-new delete pod kafka-kafka-0 --grace-period=0 --force
 kubectl -n kafka-new delete pod kafka-zookeeper-0 --grace-period=0 --force
+kubectl -n kafka-new delete pod kafka-kafka-1 --grace-period=0 --force
+kubectl -n kafka-new delete pod kafka-zookeeper-1 --grace-period=0 --force
 
 # Rollout restart the deployments
 kubectl rollout restart deployment kafka-entity-operator -n kafka-new
